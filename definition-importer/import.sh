276cfa51066b5843ec8d73f7ed0f7d95356a5d90
Djura Smits <djura.smits@gmail.com>
2021-05-25 16:02:50 +0200

    Created definition importer dockerfile

--- definition-importer/import.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pip install fhirclient==$FHIRCLIENT_VERSION click tqdm
-
-cd /mnt/definition-importer
-
-python definition-importer.py --fhirversion $FHIR_VERSION --baseurl $BASE_URL
